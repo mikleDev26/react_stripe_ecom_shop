17a3efd86a79f050f51958dbe073831b5c80254b
Mikle Chigorin <justamaizingemail@gmail.com>
2020-01-27 09:08:12 +0300

    add test firebase store

---
+++ src/firebase/test.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+// firestore
+//   .collection('users')
+//   .doc('4o2LxbJJfcVm5Rtl1Eat')
+//   .collection('cartItems')
+//   .doc('sSqgbaWrRR9KKn80RUgF');
+
+// firestore.doc('/users/4o2LxbJJfcVm5Rtl1Eat/cartItems/sSqgbaWrRR9KKn80RUgF');
+// firestore.collection('/users/4o2LxbJJfcVm5Rtl1Eat/cartItems/')
